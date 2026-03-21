a7e2f580ce3679d37693e9b29c1dd7944669a519
wolndemort <kid.cudi.1995@mail.ru>
2026-03-21 11:17:20 +0300

    proxy

--- utils/ap_logic.py
+++ utils/ap_logic.py
@@ -4,11 +4,11 @@ from config import config
 import re
 import os
 
-proxy_url = "http://user381771:wpilyj@213.139.74.73:7043"
+#proxy_url = "http://user381771:wpilyj@213.139.74.73:7043"
 
 
-os.environ['HTTP_PROXY'] = proxy_url
-os.environ['HTTPS_PROXY'] = proxy_url
+#os.environ['HTTP_PROXY'] = proxy_url
+#os.environ['HTTPS_PROXY'] = proxy_url
 genai.configure(api_key=config.GEMINI_API_KEY.get_secret_value(), transport='rest')
 model = genai.GenerativeModel('gemini-1.5-flash')
 
