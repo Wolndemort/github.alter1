bbb270b0c1e5cfc607c67a5d4d9106a6d74e11a9
wolndemort <kid.cudi.1995@mail.ru>
2026-08-05 16:38:19 +0300

    add internal nginx for alter

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -4,11 +4,12 @@ set -Eeuo pipefail
 APP_DIR="${APP_DIR:-/root/alter}"
 cd "$APP_DIR"
 
-docker compose up -d --build db redis
+docker compose up -d --build db redis alter-nginx
 docker compose run --rm migrations alembic upgrade head
 docker compose up -d --build bot
 test "$(docker inspect -f '{{.State.Health.Status}}' alter_db_container)" = healthy
 test "$(docker inspect -f '{{.State.Status}}' alter_redis_container)" = running
+test "$(docker inspect -f '{{.State.Status}}' alter_nginx)" = running
 test "$(docker inspect -f '{{.State.Status}}' alter_bot)" = running
 sleep 5
 test "$(docker inspect -f '{{.State.Status}}' alter_bot)" = running
