cb919be22787c0275ea5474598ca2d79494b1d65
wolndemort <kid.cudi.1995@mail.ru>
2026-08-14 11:20:32 +0300

    fix: clean compose orphans during deploy

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -18,9 +18,9 @@ trap diagnose ERR
 APP_DIR="${APP_DIR:-/root/alter}"
 cd "$APP_DIR"
 
-docker compose up -d --build db redis alter-nginx
+docker compose up -d --remove-orphans --build db redis alter-nginx
 docker compose run --rm migrations alembic upgrade head
-docker compose up -d --build bot media-worker
+docker compose up -d --remove-orphans --build bot media-worker
 test "$(docker inspect -f '{{.State.Health.Status}}' alter_db_container)" = healthy
 test "$(docker inspect -f '{{.State.Status}}' alter_redis_container)" = running
 test "$(docker inspect -f '{{.State.Status}}' alter_nginx)" = running
